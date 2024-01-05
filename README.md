b8d0c58511bf543f72531d4a87df08aca2928f8d
harsh <harshpriyam.official@gmail.com>
2024-01-06 02:57:48 +0530

    Data Ingestion Stage Integrated

--- README.md
+++ README.md
@@ -6,3 +6,15 @@
 # __init__.py -> This file is everywhere in folders as we are using those folders as local packages and in order to import that we have to create __init__.py
 
 # utils are the functions that we use in our code frequently
+
+## Workflows
+
+1. Update config.yaml
+2. Update schema.yaml
+3. Update params.yaml
+4. Update the entity
+5. Update the configuration manager in src config
+6. Update the components
+7. Update the pipeline
+8. Update the main.py
+9. Update the dvc.yaml
--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from src.mlProject import logger
+from src.mlProject.pipeline.stage01_data_engestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome to our custom logging")
+
+STAGE_NAME = "Data Ingestion stage"
+
+try:
+        logger.info(f">>>>> stage {STAGE_NAME} started")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx===========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
